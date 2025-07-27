b1e65ce6928efe9d37940c2f57edac68ca3308a6
Khrashaka <matic.novak@hotmail.com>
2025-07-27 20:48:31 +0200

    Create electron.d.ts

---
+++ src/types/electron.d.ts
@@ -0,0 +1,21 @@
+export interface ElectronAPI {
+    getConfig: () => Promise<any>;
+    saveConfig: (config: any) => Promise<boolean>;
+    showMessageBox: (options: any) => Promise<any>;
+    showErrorDialog: (title: string, content: string) => Promise<void>;
+    onConfigUpdate: (callback: (config: any) => void) => void;
+    onRefreshData: (callback: () => void) => void;
+    removeAllListeners: (channel: string) => void;
+  }
+  
+  declare global {
+    interface Window {
+      electronAPI: ElectronAPI;
+      versions: {
+        node: () => string;
+        chrome: () => string;
+        electron: () => string;
+        app: () => string;
+      };
+    }
+  }
